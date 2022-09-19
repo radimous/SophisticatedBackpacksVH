15c46039a4ce0bf3ead2c9739d1ba19e55fd686a
P3pp3rF1y <p3pp3rf1y@gmail.com>
2022-09-19 14:26:44 +0200

    fix: 🐛 Fixed to work with the newest versions of sophisticated backpacks and core

--- src/main/java/net/p3pp3rf1y/sophisticatedbackpacksvh/BackpackShapeProvider.java
+++ src/main/java/net/p3pp3rf1y/sophisticatedbackpacksvh/BackpackShapeProvider.java
@@ -6,14 +6,15 @@ import net.minecraft.world.level.block.Block;
 import net.minecraft.world.phys.shapes.VoxelShape;
 import net.p3pp3rf1y.sophisticatedbackpacks.backpack.BackpackShapes;
 import net.p3pp3rf1y.sophisticatedbackpacks.init.ModBlocks;
+import net.p3pp3rf1y.sophisticatedcore.util.RotatedShapes;
 
 import java.util.HashMap;
 import java.util.Map;
 
 public class BackpackShapeProvider implements BackpackShapes.IShapeProvider {
 
-	private static final Map<Block, BackpackShapes.RotatedShapes> BACKPACK_SHAPES = ImmutableMap.of(
-			ModBlocks.BACKPACK.get(), new BackpackShapes.RotatedShapes(
+	private static final Map<Block, RotatedShapes> BACKPACK_SHAPES = ImmutableMap.of(
+			ModBlocks.BACKPACK.get(), new RotatedShapes(
 					Block.box(4, 2, 6, 12, 4, 6),
 					Block.box(9.5, 0, 3.9, 12.5, 4, 5.9),
 					Block.box(3.5, 0, 3.9, 6.5, 4, 5.9),
@@ -23,22 +24,22 @@ public class BackpackShapeProvider implements BackpackShapes.IShapeProvider {
 					Block.box(4, 2, 10, 12, 4, 10),
 					Block.box(4, 2, 6, 4, 4, 10)
 			),
-			ModBlocks.IRON_BACKPACK.get(), new BackpackShapes.RotatedShapes(
+			ModBlocks.IRON_BACKPACK.get(), new RotatedShapes(
 					Block.box(6, 0, 6, 10, 5, 9),
 					Block.box(7.5, 2.8, 8.4, 8.5, 3.8, 9.4)
 			),
-			ModBlocks.GOLD_BACKPACK.get(), new BackpackShapes.RotatedShapes(
+			ModBlocks.GOLD_BACKPACK.get(), new RotatedShapes(
 					Block.box(3, 0, 6.5, 7, 6, 9.5),
 					Block.box(4.5, 3.8, 8.9, 5.5, 4.8, 9.9),
 					Block.box(10.5, 3.8, 8.9, 11.5, 4.8, 9.9),
 					Block.box(9, 0, 6.5, 13, 6, 9.5)
 			),
-			ModBlocks.DIAMOND_BACKPACK.get(), new BackpackShapes.RotatedShapes(
+			ModBlocks.DIAMOND_BACKPACK.get(), new RotatedShapes(
 					Block.box(4, 0, 5, 12, 6, 10),
 					Block.box(4, 6, 5, 12, 7, 11),
 					Block.box(7, 2, 10, 9, 6, 11)
 			),
-			ModBlocks.NETHERITE_BACKPACK.get(), new BackpackShapes.RotatedShapes(
+			ModBlocks.NETHERITE_BACKPACK.get(), new RotatedShapes(
 					Block.box(4, 6, 4, 12, 12, 9),
 					Block.box(4, 12, 4, 12, 13, 10),
 					Block.box(7, 8, 9, 9, 12, 10),
